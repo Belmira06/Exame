f163263ea01ea4e63f1a08d37b2deba102327c48
Belmira06 <belmirasantosbelmia@gmail.com>
2024-07-02 22:02:05 +0100

    Delete exame directory

--- exame/src/main/java/ao/com/fazendo/api/exame/BEL/IUsuario.java
+++
@@ -1,9 +0,0 @@
-package ao.com.fazendo.api.exame.BEL;
-
-import org.springframework.data.repository.CrudRepository;
-
-import ao.com.fazendo.api.exame.model.Usuario;
-
-public interface IUsuario extends CrudRepository<Usuario, Integer>{
-	
-	}
